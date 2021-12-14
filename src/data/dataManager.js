30dae1999e05b55be97c0827614719ec4814d652
Rémi <roeland.remi@yahoo.fr>
2021-12-14 12:48:42 +0100

    Add JsDoc

--- src/data/dataManager.js
+++ src/data/dataManager.js
@@ -1,23 +1,35 @@
 import data from '../data/data.json';
 
+/**
+ * Return the full list of housings in the form of simplified object containing id, title and cover
+ *
+ * @return  {Array.<Object>}
+ */
 function getHousingList() {
-  const HousingList = []
+  const housingList = []
   data.forEach((housing) => {
-    HousingList.push({
+    housingList.push({
       id: housing.id,
       title: housing.title,
       cover: housing.cover,
     });
   })
-  return HousingList;
+  return housingList;
 }
 
-function getHousingData(id){
+/**
+ * Return the data of one housing
+ *
+ * @param   {Number|String}  id  Requested housing id
+ *
+ * @return  {Object}      Housing data
+ */
+function getHousingData(id) {
   const result = data.find((housing) => housing.id === id);
   return !result ? null : result;
 }
 
 export {
-  getHousingList, 
+  getHousingList,
   getHousingData
 };
